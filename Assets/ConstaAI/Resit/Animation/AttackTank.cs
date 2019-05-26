4cae8fd47a8e69cd9250b9fd55be5aa7c4c7530b
Constantinos Iasonides <constantinosiasonides@gmail.com>
2019-05-26 14:30:06 +0200

    change the tank code doesnt read in the game

--- Assets/ConstaAI/Resit/Animation/AttackTank.cs
+++ Assets/ConstaAI/Resit/Animation/AttackTank.cs
@@ -13,32 +13,48 @@ public class AttackTank : StateMachineBehaviour
     TankInfo tankInfo;
 
     int shootableLayer;
-    float TillFire;
+    float tillFire;
     float afterFire;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        tank = animator.gameObject;
         waypointList = GameObject.FindGameObjectsWithTag("waypoint");
         tankInfo = animator.gameObject.GetComponent<TankInfo>();
-        tank = animator.gameObject;
         anim = animator;
         agent = tank.GetComponent<NavMeshAgent>();
         shootableLayer = LayerMask.GetMask("Shootable");
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        afterFire += Time.deltaTime;
+
+        if(afterFire >= tillFire)
+        {
+            Fire();
+        }
+    }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
 
+    void Fire()
+    {
+        RaycastHit hit;
+
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(tankInfo.fireTransform.transform.position, tankInfo.fireTransform.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, shootableLayer))
+        {
+            Debug.DrawRay(tankInfo.fireTransform.transform.position, tankInfo.fireTransform.transform.TransformDirection(Vector3.forward) * hit.distance, Color.green);
+            Debug.Log("fireeee");
+        }
+    }
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     //{
